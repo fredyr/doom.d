ceb543f2c9752ebc4d116ae1a150bf2ec4017f31
Fredrik Dyrkell <fredrik.dyrkell@1928diagnostics.com>
2022-02-28 10:24:42 +0100

    doom config

---
+++ config.el
@@ -0,0 +1,136 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Fredrik Dyrkell"
+      user-mail-address "fredrik.dyrkell@1928diagnostics.com")
+
+(cond (IS-MAC
+       (setq mac-command-modifier      'control
+             mac-option-modifier       'meta
+             mac-right-option-modifier 'alt)))
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "DejaVu Sans Mono" :size 15 :weight 'normal)
+      doom-variable-pitch-font (font-spec :family "DejaVu Sans Mono" :size 16))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-fd)
+(setq doom-fd-brighter-comments t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Documents/projects/1928/docs")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;; (add-load-path! "~/.doom.d/lisp/emacs-evil-colemak-basics")
+;; (use-package! evil-colemak-basics
+;;   :after evil
+;;   :config
+;;   ;(setq evil-colemak-basics-rotate-t-f-j t)
+;;   )
+;; (after! evil (global-evil-colemak-basics-mode))
+;; (setq evil-colemak-basics-layout-mod 'mod-dh)
+
+(defun append-semicolon ()
+  (interactive)
+  (evil-end-of-line)
+  (evil-append nil)
+  (insert ";")
+  (evil-normal-state))
+
+(define-key evil-motion-state-map "H" 'evil-first-non-blank)
+(define-key evil-motion-state-map "L" 'evil-end-of-line)
+
+(global-set-key (kbd "C-<left>") 'sp-forward-barf-sexp)
+(global-set-key (kbd "C-<right>") 'sp-forward-slurp-sexp)
+(global-set-key (kbd "C-;") 'append-semicolon)
+
+(setq evil-move-cursor-back nil)
+(setq evil-move-beyond-eol t)
+
+(add-hook! smartparens-mode
+  (smartparens-strict-mode)
+  (electric-pair-mode))
+
+;; https://github.com/hlissner/doom-emacs/issues/478
+(after! smartparens
+  (dolist (brace '("(" "{" "["))
+    (sp-pair brace nil :unless '(:rem sp-point-before-word-p sp-point-before-same-p))
+    ;; (sp-with-modes 'org-mode
+    ;;   (sp-local-pair "<" nil :actions :rem))
+    ))
+
+
+;; python black settings
+;; taken from https://gist.github.com/jordangarrison/8720cf98126a1a64890b2f18c1bc69f5
+;; (use-package! python-black
+;;   :demand t
+;;   :after python)
+;; (add-hook! 'python-mode-hook #'python-black-on-save-mode)
+;; (setq blacken-line-length 100)
+;; (setq blacken-skip-string-normalization t)
+
+(after! python
+  (setq python-black-extra-args '("--skip-string-normalization" "--line-length" "100"))
+  (setq python-shell-interpreter "python3")
+  (setq lsp-pylsp-plugins-pydocstyle-enabled nil)
+  (setq lsp-pylsp-plugins-pyflakes-enabled nil)
+  (setq lsp-pylsp-plugins-flake8-enabled t)
+  )
+
+;; (add-hook 'c-common-mode-hook
+;;   (lambda ()
+;;     (add-hook (make-local-variable 'before-save-hook)
+;;               'clang-format-buffer)))
+;; (map! :leader :desc "Comment line" "c l " #'comment-line)
+;; (add-hook! 'before-save-hook #'+format/buffer)
+;; (remove-hook! 'before-save-hook #'+format/buffer)
+;;
+(setq abbrev-file-name "~/.emacs.d/.abbrev_defs")
+
+(setq company-idle-delay 1.0)
+;; (setq doom-localleader-key ",")
+;; (setq doom-localleader-alt-key "M-,")
+;;
+;; https://stackoverflow.com/questions/16770868/org-babel-doesnt-load-graphviz-editing-mode-for-dot-sources
+;; https://orgmode.org/worg/org-contrib/babel/languages/ob-doc-dot.html
+;;
+(require 'ob-dot)
+(add-to-list 'org-src-lang-modes '("dot" . graphviz-dot))
+(org-babel-do-load-languages 'org-babel-load-languages '((dot . t)))
---
+++ themes/doom-fd-theme.el
@@ -0,0 +1,182 @@
+;; doom-fd-theme.el --- a more fd version of doom-one -*- lexical-binding: t; no-byte-compile: t; -*-
+;;; Commentary:
+(require 'doom-themes)
+;;; Code:
+;;
+(defgroup doom-fd-theme nil
+  "Options for the `doom-fd' theme."
+  :group 'doom-themes)
+
+(defcustom doom-fd-brighter-modeline nil
+  "If non-nil, more vivid colors will be used to style the mode-line."
+  :group 'doom-fd-theme
+  :type 'boolean)
+
+(defcustom doom-fd-brighter-comments nil
+  "If non-nil, comments will be highlighted in more vivid colors."
+  :group 'doom-fd-theme
+  :type 'boolean)
+
+(defcustom doom-fd-comment-bg doom-fd-brighter-comments
+  "If non-nil, comments will have a subtle, darker background.
+Enhancing their legibility."
+  :group 'doom-fd-theme
+  :type 'boolean)
+
+(defcustom doom-fd-padded-modeline doom-themes-padded-modeline
+  "If non-nil, adds a 4px padding to the mode-line.
+Can be an integer to determine the exact padding."
+  :group 'doom-fd-theme
+  :type '(choice integer boolean))
+
+
+;;
+(def-doom-theme doom-fd
+  "A dark theme based off of xero's fd VIM colorscheme"
+
+  ((bg         '("#242424"))
+   (bg-alt     '("#222222"))
+   (base0      '("#1d2127"))
+   (base1      '("#1d2127"))
+   (base2      '("#272727"))
+   (base3      '("#32353f"))
+   (base4      '("#494952"))
+   (base5      '("#62686E"))
+   (base6      '("#757B80"))
+   (base7      '("#9ca0a4"))
+   (base8      '("#faf4c6"))
+   (fg         '("#f6f3e8"))
+   (fg-alt     '("#5D656B"))
+
+   (grey       '("#686858"))
+   (red        '("#aa4450"))
+   (orange     '("#ff9800"))
+   (green      '("#87875f"))
+   (green-br   '("#719611"))
+   (teal       '("#578F8F" "#44b9b1" ))
+   (yellow     '("#cc8800"           ))
+   (blue        "#8ac6f2")
+   ;; (blue       '("#87AFD7"           ))
+   (dark-blue  '("#6688aa"           ))
+   (magenta    '("#8787AF"           ))
+   (violet     '("#8181a6"           ))
+   (cyan       '("#87ceeb"           ))
+   (dark-cyan  '("#528b8b"           ))
+
+   (fd-red           "#e5786d")
+   (fd-blue          "#8ac6f2")
+   (fd-light-yellow  "#ffa")
+   (fd-yellow        "#ff5")
+   (fd-green         "#95e454")
+   (fd-comments      "#99968b")
+   (fd-highlight     "#454545")
+   ;; (fd-type          "#cdcde0")
+   (fd-type (doom-lighten fd-blue 0.5))
+
+   ;; face categories
+   (highlight      cyan)
+   (vertical-bar   base0)
+   (selection      base8)
+   (builtin        fd-light-yellow)
+   (comments       (if doom-fd-brighter-comments fd-comments grey))
+   (doc-comments   (if doom-fd-brighter-comments (doom-lighten fd-comments 0.0) (doom-darken grey 0.1)))
+   (constants      fd-red)
+   (functions      fd-yellow)
+   (keywords       fd-blue)
+   (methods        fd-yellow)
+   (operators      green-br)
+   (type           fd-type)
+   (strings        fd-green)
+   (variables      fd-light-yellow)
+   (numbers        fd-red)
+   (region         base4)
+   (error          red)
+   (warning        orange)
+   (success        green)
+   (vc-modified    yellow)
+   (vc-added       green)
+   (vc-deleted     red)
+
+   ;; custom categories
+   (hidden     `(,(car bg) "black" "black"))
+   (hidden-alt `(,(car bg-alt) "black" "black"))
+   (-modeline-pad
+    (when doom-fd-padded-modeline
+      (if (integerp doom-fd-padded-modeline) doom-fd-padded-modeline 4)))
+
+   (modeline-fg     "#bbc2cf")
+   (modeline-fg-alt (doom-blend blue grey (if doom-fd-brighter-modeline 0.4 0.08)))
+
+   (modeline-bg
+    (if doom-fd-brighter-modeline
+        modeline-bg
+      `(,(doom-darken (car bg) 0.15) ,@(cdr base1))))
+   (modeline-bg-l
+    (if doom-fd-brighter-modeline
+        `("#383f58" ,@(cdr base1))
+      `(,(car base3) ,@(cdr base0))))
+   (modeline-bg-inactive   `(,(doom-darken (car bg-alt) 0.2) ,@(cdr base0)))
+   (modeline-bg-inactive-l (doom-darken bg 0.20)))
+
+  ;;;; Base theme face overrides
+  ((cursor :background "yellow")
+   ((font-lock-comment-face &override)
+    :background (if doom-fd-comment-bg (doom-darken bg-alt 0.095)))
+   ;; (font-lock-keyword-face :foreground fd-blue :bold bold)
+   (fringe :background base2)
+   ((line-number &override) :foreground base4)
+   ((line-number-current-line &override) :foreground blue :bold bold)
+   (mode-line
+    :background base3 :foreground modeline-fg
+    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,base3)))
+   (mode-line-inactive
+    :background bg-alt :foreground modeline-fg-alt
+    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-inactive)))
+   (mode-line-emphasis :foreground (if doom-fd-brighter-modeline base8 highlight))
+   (mode-line-buffer-id :foreground green-br :bold bold)
+
+   ;;;; company
+   (company-tooltip-selection     :background base3)
+   ;;;; css-mode <built-in> / scss-mode
+   (css-proprietary-property :foreground orange)
+   (css-property             :foreground green)
+   (css-selector             :foreground blue)
+   ;;;; doom-modeline
+   (doom-modeline-bar :background (if doom-fd-brighter-modeline modeline-bg highlight))
+   (doom-modeline-buffer-path :foreground (if doom-fd-brighter-modeline base8 blue) :bold bold)
+   ;;;; elscreen
+   (elscreen-tab-other-screen-face :background "#353a42" :foreground "#1e2022")
+   ;;;; markdown-mode
+   (markdown-header-face :inherit 'bold :foreground red)
+   ;;;; org <built-in>
+   ((org-block &override) :background bg-alt)
+   ((org-block-begin-line &override) :background bg-alt)
+   ((org-block-end-line &override) :background bg-alt)
+   (org-hide :foreground hidden)
+   ;;;; rainbow-delimiters
+   (rainbow-delimiters-depth-1-face :foreground dark-cyan)
+   (rainbow-delimiters-depth-2-face :foreground teal)
+   (rainbow-delimiters-depth-3-face :foreground dark-blue)
+   (rainbow-delimiters-depth-4-face :foreground green)
+   (rainbow-delimiters-depth-5-face :foreground violet)
+   (rainbow-delimiters-depth-6-face :foreground green)
+   (rainbow-delimiters-depth-7-face :foreground orange)
+   ;;;; rjsx-mode
+   (rjsx-attr :foreground magenta :slant 'italic :weight 'medium)
+   (rjsx-tag :foreground blue)
+   (rjsx-tag-bracket-face :foreground base8)
+   ;;;; solaire-mode
+   (solaire-mode-line-face
+    :inherit 'mode-line
+    :background modeline-bg-l
+    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-l)))
+   (solaire-mode-line-inactive-face
+    :inherit 'mode-line-inactive
+    :background modeline-bg-inactive-l
+    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-inactive-l))))
+
+  ;;;; Base theme variable overrides
+  ;; ()
+  )
+
+;;; doom-fd-theme.el ends here
